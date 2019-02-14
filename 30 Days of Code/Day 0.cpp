fee46a10eb063018adfa6e8bd9b015569ae7084a
ankurgupta255 <43845288+ankurgupta255@users.noreply.github.com>
2019-02-14 17:37:39 +0530

    Create Day 0.cpp

---
+++ 30 Days of Code/Day 0.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    char str[100];
+    gets(str);
+    cout<<"Hello, World.";
+    cout<<endl;
+    cout<<str;
+}
+
+
