b1e298ddd6a25b8b2ea66ca9111db5ff831dba07
Ankur Gupta <gupta.ankur255@gmail.com>
2019-05-19 00:34:23 +0530

    Create Day 15.cpp

---
+++ 30 Days of Code/Day 15.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          Node* temp;
+          Node* tail= new Node(data);
+          if(head==NULL){
+              head=tail;
+              return head;
+          }
+          temp=head;
+          while(temp->next!=NULL){
+              temp=temp->next;
+          }
+          temp->next=tail;
+          return head;   
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
